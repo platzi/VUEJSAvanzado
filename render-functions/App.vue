ff80e0c198912a9fe7863e2e467fecdd8ce321f8
Javier Diaz <hello@coderdiaz.me>
2019-02-14 12:02:14 -0500

    feat: added render-functions initial prototype

---
+++ render-functions/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <button>Toggle Modal</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+body {
+  background: #cecece;
+}
+.modal {
+  position: absolute;
+  top: 50%;
+  left:50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+  width: 250px;
+  background: #fff;
+  padding: 15px;
+}
+.modal > .message {
+  margin: 0;
+  margin-bottom: 10px;
+}
+</style>
