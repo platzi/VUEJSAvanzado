7f303e45339f8e21f10049ddba9f296e4bea034e
Javier Diaz <hello@coderdiaz.me>
2019-02-15 14:53:00 -0500

    feat: firebase install and config

--- platzi-rooms/src/main.js
+++ platzi-rooms/src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyDN6XptK-8PRvR6Sr0Bpd2IJFMlTi2wonY',
+  authDomain: 'platzi-rooms-b9291.firebaseapp.com',
+  databaseURL: 'https://platzi-rooms-b9291.firebaseio.com',
+  projectId: 'platzi-rooms-b9291',
+  storageBucket: 'platzi-rooms-b9291.appspot.com',
+  messagingSenderId: '31419217426',
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
