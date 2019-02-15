df2f92b3f4ebafaef8ae954e3fd39432b3851cda
Javier Diaz <hello@coderdiaz.me>
2019-02-14 21:46:13 -0500

    feat: toggle-input integration

--- platzi-rooms/src/layouts/DefaultLayout.vue
+++ platzi-rooms/src/layouts/DefaultLayout.vue
@@ -40,14 +40,18 @@
         <div class="mb-4">
           <label class="input__label">Email</label>
           <div class="form__field relative">
-            <input class="input__field" type="text" placeholder="bruce.wayne@imnotbatman.org">
+            <input v-model="formLogin.email" class="input__field" type="text" placeholder="bruce.wayne@imnotbatman.org">
           </div>
         </div>
         <div class="mb-4">
           <label class="input__label">Password</label>
           <div class="form__field relative">
-            <input class="input__field" type="password" placeholder="*********">
+            <input v-model="formLogin.password" class="input__field" type="password" placeholder="*********">
           </div>
+        </div>
+        <div class="mb-4">
+          <toggle-input v-model="formLogin.rememberMe"></toggle-input>
+          Remember Me
         </div>
         <div class="mb-4">
           <button class="btn btn-primary mr-3 w-full">Login</button>
@@ -62,9 +66,19 @@ import { mapGetters } from 'vuex';
 import HeaderPartial from '@/partials/HeaderPartial.vue';
 import FooterPartial from '@/partials/FooterPartial.vue';
 import Modal from '@/components/Modal.vue';
+import ToggleInput from '@/components/ToggleInput.vue';
 
 export default {
   name: 'DefaultLayout',
+  data() {
+    return {
+      formLogin: {
+        email: '',
+        password: '',
+        rememberMe: false,
+      },
+    };
+  },
   computed: {
     ...mapGetters([
       'modals',
@@ -74,6 +88,7 @@ export default {
     HeaderPartial,
     FooterPartial,
     Modal,
+    ToggleInput,
   },
   methods: {
     closeModal () {
