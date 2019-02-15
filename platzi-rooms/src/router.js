b5988f1e186f0d5aced36deff87287d1ea67ff22
Javier Diaz <hello@coderdiaz.me>
2019-02-15 11:38:39 -0500

    feat: creating a new post page

--- platzi-rooms/src/router.js
+++ platzi-rooms/src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import HomePage from './views/HomePage.vue';
 import SearchPage from './views/SearchPage.vue';
 import NotFoundPage from './views/NotFoundPage.vue';
+import CreateHousePage from './views/CreateHousePage.vue';
 
 // User Pages
 import ProfilePage from './views/user/ProfilePage.vue';
@@ -37,6 +38,15 @@ export default new Router({
       path: '/user/houses',
       name: 'HousesPages',
       component: HousesPages,
+    },
+    {
+      path: '/house',
+      redirect: { name: 'ProfilePage' },
+    },
+    {
+      path: '/house/new',
+      name: 'CreateHousePage',
+      component: CreateHousePage,
     },
     {
       path: '*',
---
+++ platzi-rooms/src/views/CreateHousePage.vue
@@ -0,0 +1,54 @@
+<template>
+  <page-layout>
+    <section class="py-4 bg-teal-dark">
+      <div class="container">
+        <form class="form">
+          <div class="form__field relative">
+            <i class="input-icon material-icons absolute text-grey-darker">search</i>
+            <input
+              class="input__search"
+              id="where"
+              type="text"
+              placeholder="Mexico City, Mexico">
+          </div>
+        </form>
+      </div>
+    </section>
+    <section class="section__create py-6">
+      <div class="container">
+        <h1 class="text-3xl">Publish a new room</h1>
+        <form>
+          <div class="mb-4">
+            <label class="input__label">Title</label>
+            <input class="input__field" type="text" placeholder="Bruce Wayne">
+          </div>
+          <div class="mb-4">
+            <label class="input__label">Description</label>
+            <textarea class="input__field" rows="10" placeholder="Bruce Wayne"></textarea>
+          </div>
+          <div class="mb-4">
+            <label class="input__label">Featured Image</label>
+            <input class="input__field" type="text" placeholder="https://images.unsplash.com/photo-1432303492674-642e9d0944b">
+          </div>
+          <div class="mb-4 text-right">
+            <button class="w-full bg-yellow-dark text-yellow-darker font-semibold py-3 px-6 rounded">
+              Publish
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  </page-layout>
+</template>
+
+<script>
+import PageLayout from '@/layouts/PageLayout.vue';
+
+export default {
+  name: 'CreateHousePage',
+  component: {
+    PageLayout,
+  },
+};
+</script>
+
