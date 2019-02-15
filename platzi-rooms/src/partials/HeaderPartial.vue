b95a6583da56fa7c31ee25785fa3bf27ccfcbd78
Javier Diaz <hello@coderdiaz.me>
2019-02-15 10:50:32 -0500

    feat: vuex

--- platzi-rooms/src/partials/HeaderPartial.vue
+++ platzi-rooms/src/partials/HeaderPartial.vue
@@ -9,7 +9,7 @@
         </div>
         <div class="flex items-center w-auto">
           <div class="items__controls">
-            <div class="flex" v-if="false">
+            <div class="flex" v-if="user">
               <button class="mr-2 flex items-center">
                 <i class="material-icons">add</i>
               </button>
@@ -19,7 +19,7 @@
               <div class="flex items-center">
                 <img class="w-8 h-8 rounded-full mr-2" src="https://avatars2.githubusercontent.com/u/1901273?s=460&v=4" alt="Avatar of Javier Diaz">
                 <div class="text-sm">
-                  <p class="text-black leading-none">Javier Diaz</p>
+                  <p class="text-black leading-none">{{ user.name }}</p>
                   <p class="text-grey-dark">Online</p>
                 </div>
               </div>
@@ -38,6 +38,8 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'HeaderPartial',
   data() {
@@ -55,6 +57,11 @@ export default {
     signUp() {
       console.log('Sign Up Click');
     },
+  },
+  computed: {
+    ...mapGetters({
+      user: 'authUser',
+    }),
   },
 };
 </script>
--- platzi-rooms/src/store.js
+++ platzi-rooms/src/store.js
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import sourceData from './data.json';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    ...sourceData,
     user: null,
+    authId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1',
     modals: {
       login: false,
     },
@@ -22,5 +25,6 @@ export default new Vuex.Store({
   },
   getters: {
     modals: state => state.modals,
+    authUser: state => state.users[state.authId],
   },
 });
